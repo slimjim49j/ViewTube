3e7efe78c86a0142f0cd7271391ee1b63e81caeb
Jimmy <slimjim49j@gmail.com>
2020-03-10 10:47:19 -0700

    Add links to all videos in home page

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Home extends React.Component {
     constructor(props) {
@@ -12,7 +13,14 @@ class Home extends React.Component {
     render() {
         return(
             <div>
-                
+                hi
+                <ul>
+                    {this.props.videos ? (
+                        Object.values(this.props.videos).map( video => <li><Link to={`video/${video.id}`}>{video.title}</Link></li>) 
+                    ) : <p>no videos</p>
+                    }
+
+                </ul>
             </div>
         )
     }
