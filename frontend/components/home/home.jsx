c59d27b31321173e99e45d3ec7c4a688bf6136ee
Jimmy <slimjim49j@gmail.com>
2020-03-13 10:00:16 -0700

    Add video preview

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -10,12 +10,20 @@ class Home extends React.Component {
         this.props.fetchAllVideos();
     }
 
+    play(e) {
+        e.currentTarget.play();
+    }
+
+    pause(e) {
+        e.currentTarget.pause();
+    }
+
     render() {
         return(
             <div>
                 <main className="home-vid-container">
                     {this.props.videos ? (
-                        Object.values(this.props.videos).map( video => <Link to={`video/${video.id}`} className="home-vid" key={video.id}><video src={video.video}></video> {video.title}</Link>) 
+                        Object.values(this.props.videos).map( video => <Link to={`video/${video.id}`} className="home-vid" key={video.id}><video src={video.video} onMouseOver={this.play} onMouseOut={this.pause} ></video> {video.title}</Link>) 
                     ) : <p>no videos</p>
                     }
 
