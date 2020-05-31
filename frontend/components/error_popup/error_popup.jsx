f2bbc893105ebf7ee0f57d2ebd8f78e13603d3f0
Jimmy <slimjim49j@gmail.com>
2020-05-31 10:52:18 -0700

    Tweak error popup to close when link or x is clicked

--- frontend/components/error_popup/error_popup.jsx
+++ frontend/components/error_popup/error_popup.jsx
@@ -4,13 +4,18 @@ import { Link } from 'react-router-dom';
 class ErrorPopup extends Component {
     constructor(props) {
         super(props);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     componentDidUpdate() {
         const status = this.props.errorPopupStatus;
         if (status) {
-            setTimeout(() => (this.props.receiveErrorPopupStatus(false)), 7000);
+            // setTimeout(() => (this.props.receiveErrorPopupStatus(false)), 7000);
         }
+    }
+
+    handleClick() {
+        this.props.receiveErrorPopupStatus(false)
     }
     
     render() {
@@ -19,8 +24,9 @@ class ErrorPopup extends Component {
             <div className="error-popup-container">
                 
                 <p>
-                    <svg fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg>
-                    <Link to={`/login?destination=${this.props.path}`}>
+                    <span className="close" onClick={this.handleClick}></span>
+                    {/* <svg fill="currentColor" focusable="false" width="16px" height="16px" viewBox="0 0 24 24" xmlns="https://www.w3.org/2000/svg"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm1 15h-2v-2h2v2zm0-4h-2V7h2v6z"></path></svg> */}
+                    <Link onClick={this.handleClick} to={`/login?destination=${this.props.path}`}>
                         Please login to perform that action
                     </Link>
                 </p>
